76d8bcc53ea5c1deca1118bab10054fe512fa229
Christopher Clothier <c.r.clothier@gmail.com>
2021-12-11 11:53:20 +0000

    Remove placholder tasks

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import './style.css';
 import { addTask, createList } from './addtask.js';
 
-let items = [
-  {
-    description: 'Eat hamburgers',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Steal neighbours dog',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'Become millionaire',
-    completed: false,
-    index: 2,
-  },
-];
+let items = [];
 
 const getLocalData = () => {
   if (localStorage.getItem('localData')) {
